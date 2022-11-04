f10ae93e1d5d00de4dad4761e529e20351fe0627
Joong-Young, Eom <joongyoung.eom@colubrismx.com>
2022-11-04 16:56:10 +0900

    update logic

--- TwinCAT Project4/Untitled1/Motor.h
+++ TwinCAT Project4/Untitled1/Motor.h
@@ -1,33 +1,109 @@
 #pragma once
+
+
+#include "UserInterface.h"
 #include <vector>
+
+class CModule1;
 
 
 class Motor
 {
 
 private:
-
 	const static  UINT8 m_mode = 1;
-	UINT16 m_status_word;  
+	UINT16 m_status_word;
 	long m_deceleration;
 	long m_acceleration;
 	long m_velocity;
+	long m_target_position;
 	long m_current_position;
+	int m_command;
+	
+
+
+protected:
 
 public:
+	typedef bool (CModule1::*fp)(Motor&);
+	int m_num;
+	int m_timeout_count;
+	int m_depth;
+
+	//bool(*p)(void);
+	fp* m_execute;
+	fp* m_re_execute;
+	int m_set_point_ack_flag;
+	int m_goal;
+	int m_seq;
+	int m_state;
+	int m_key;
+
+	bool set_position;
+
 	Motor() : m_status_word(0x00),
-			  m_deceleration(6000),
-			  m_acceleration(5000),
-			  m_velocity(3000),
-			  m_current_position(0){}
+		m_deceleration(6000),
+		m_acceleration(5000),
+		m_velocity(3000),
+		m_target_position(0),
+		m_current_position(0),
+		m_command(0),
+		m_timeout_count(0),
+		m_num(0),
+		m_seq(0),
+		m_goal(0),
+		m_set_point_ack_flag(0),
+		m_depth(1),
+		set_position(false),
+		m_key(0){}
 
-	
-	void UpdateCurrentPosition(long position) { m_current_position = position; };
-
-	long GetCurrentPosition() { return m_current_position; }
 	UINT8 GetMode() { return m_mode; }
-	long GetAcceleration() {return m_acceleration;}
+
+	long GetAcceleration() { return m_acceleration; }
 	long GetDeceleration() { return m_deceleration; }
 	long GetVelocity() { return m_velocity; }
+	long GetTargetPosition() { return m_target_position; }
+	long GetCurrentPosition() { return m_current_position; }
+	int  GetCommand() { return m_command; }
+
+	void SetNum(int num) { this->m_num = num; }
+	void SetTargetPosition(long val) { m_target_position = val; }
+	void SetCommand(int cmd) { m_command = cmd; };
+	void SetCurrentPosition() { m_current_position = m_target_position;  };
+
+	int GetKey() {
+		
+		//fault
+		if ((m_key % 100) / 10 == 8) {
+			m_key += 1;
+			return m_key;
+		}
+		else if(m_key == 241){
+			m_key += 1;
+			return m_key;
+		}
+		return 100 * m_command + 10 * m_state + 1;
+	}
+	void SetKey(int key) {
+		m_key = key;
+	}
+	int GetDepth() {
+
+		if (set_position)
+			m_depth = 2;
+		
+		return m_depth;
+	}
+	
+	void update(Module1DataArea3 data ) {
+
+		m_command = data.command[m_num];
+		m_target_position = data.target_position[m_num];
+	
+		//data.command;
+		//this->m_target_position = data.target_position;
+	}
+
 
 };
+
--- TwinCAT Project4/Untitled1/UserInterface.h
+++ TwinCAT Project4/Untitled1/UserInterface.h
@@ -1,16 +1,22 @@
 #pragma once
 
 
-struct state {
+typedef struct _state {
 	static constexpr INT servo_off = 1;
-	static constexpr INT servo_on = 2;
+	//static constexpr INT servo_on = 2;
 	static constexpr INT set_position = 3;
-	static constexpr INT recovery = 4;
-	static constexpr INT standby = 5;
-	static constexpr INT error = 6;
-};
+	static constexpr INT move = 4;
+	static constexpr INT recovery = 5;
+	static constexpr INT standby = 6;
+	static constexpr INT error = 7;
+ 
+	/*temp*/
+	static constexpr INT enable = 10;
+	static constexpr INT set = 8;
+	static constexpr INT stop = 9;
+}state;
 
-struct command {
+typedef struct _command {
 	static constexpr USHORT shutdown = 0x06;
 	static constexpr USHORT switch_on = 0x07;
 	static constexpr USHORT switch_on_and_enable_oepration = 0x3F;
@@ -19,4 +25,33 @@ struct command {
 	static constexpr USHORT disable_operation = 0x07;
 	static constexpr USHORT enable_operation = 0x0F;
 	static constexpr USHORT fault_reset = 0x80;
-};
+}command;
+
+typedef struct _status_word {
+
+	static constexpr USHORT mask_type1 = 0x004F;
+	static constexpr USHORT mask = 0x006F;
+
+	static constexpr USHORT mask2 = 0x146F;
+
+	static constexpr USHORT mask_type3 = 0x0400;
+	static constexpr USHORT mask_set_point_ack = 0x0400;
+
+
+	static constexpr USHORT switch_on_disabled = 0x0040;
+	static constexpr USHORT ready_to_switch_on = 0x0021;
+	static constexpr USHORT switch_on = 0x0023;
+	static constexpr USHORT operation_enabled = 0x0027;
+	static constexpr USHORT fault = 0x0008;
+	static constexpr USHORT fault_reaction_active = 0x000F;
+	static constexpr USHORT quick_stop_active = 0x0007;
+
+
+	static constexpr USHORT target_reached = 0x0400;
+	static constexpr USHORT set_point_ack = 0x1000;
+
+
+}status_word;
+
+
+
